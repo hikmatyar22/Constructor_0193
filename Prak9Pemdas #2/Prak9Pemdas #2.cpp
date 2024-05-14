3b59af0ff697bd96cb849052622bd9cfe94d22a1
hikmatyar22 <abiq5161@gmail.com>
2024-05-14 10:46:22 +0700

    membuat class mahasiswa dengan bebrapa member yang bersifat pribate dan public

---
+++ Prak9Pemdas #2/Prak9Pemdas #2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*Defenisi Function*/}
+	static int getNim(){ return nim; /*Defenisi Function*/}
+
+	Mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+
+
+
+
+int main()
+{
+	
+}
