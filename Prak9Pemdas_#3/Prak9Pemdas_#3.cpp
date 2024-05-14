323cf95122ae728c150b95f681677257b53951c4
hikmatyar22 <abiq5161@gmail.com>
2024-05-14 11:42:15 +0700

    memmasukan 2 konstruksi di class mahasiswa

--- Prak9Pemdas_#3/Prak9Pemdas_#3.cpp
+++ Prak9Pemdas_#3/Prak9Pemdas_#3.cpp
@@ -8,15 +8,27 @@ private:
 	int nim;
 	string nama;
 public:
-	Mahasiswa();
-	Mahasiswa(int);
+
+	Mahasiswa() {
+		nim = 0;
+		nama = "";
+	}
+
+	Mahasiswa(int iNim) {
+		nim = iNim;
+	}
+
 	Mahasiswa(string);
 	Mahasiswa(int iNim, string iNama);
 	void cetak();
+
 };
+
+
 
 
 int main()
 {
-	
+
+
 }
