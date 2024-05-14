bd61a57f41439e02fcc4154a2aea8df701fa7ea3
hikmatyar22 <abiq5161@gmail.com>
2024-05-14 10:14:04 +0700

    membuat beberapa member yang bersifat public di class mahasiswa

--- PRAK9Pemdas/PRAK9Pemdas.cpp
+++ PRAK9Pemdas/PRAK9Pemdas.cpp
@@ -2,9 +2,19 @@
 #include <string>
 using namespace std;
 
-class mahasiswa {
+class Mahasiswa {
 
+public:
+	static int nim;
+	int id;
+	string nama;
 
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
 };
 
 
